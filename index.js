4eb37ca6de59223b7448b9fcd2733b8d37197e91
DanielVitorIFG <danielv759153@gmail.com>
2020-01-22 15:44:29 -0300

    Dependências instaladas

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 // Dizendo ao Express usar o EJS como view engine.
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req,res) => {
     res.render('index'); 
