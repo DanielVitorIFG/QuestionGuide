f2737fb6553b16fbd0041ca335089af863b0cc09
DanielVitorIFG <danielv759153@gmail.com>
2020-01-22 17:35:11 -0300

    Página para perguntar concluída

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ app.get('/', (req,res) => {
     res.render('index'); 
 });
 
+app.get('/perguntar', (req,res) => {
+    res.render('perguntar'); 
+});
+
+
 app.listen(8080, () => console.log('Software rodando'));
